b4c5ccfbd876e8a8dca9863db042e3fed30034e1
g19g17 <g19g17melo@gmail.com>
2023-01-21 08:43:34 -0300

    add README.md and script.js update

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1 align="center"> Habits </h1>
+
+<p align="center">
+NLW é um evento exclusivo e gratuito, promovido pela Rocketseat para ensino de tecnologias WEB.
+</p>
+
+## 📗️ O que é esse Projeto?:
+
+---
+
+É um projeto que serve para salvar seus habitos do dia-a-dia mostrando para você seus habitos feitos por você no dia anterior e hoje!
+
+## 👀️ Preview:
+
+---
+
+<p align="center">
+    <img alt="projeto Habits" src=".github/preview.png">
+</p>
+
+## 💻️ Tecnologias:
+
+---
+
+* HTML
+* CSS
+* JavaScript
+* Git & GitHub
+* Figma
+
--- script.js
+++ script.js
@@ -16,6 +16,37 @@ function add() {
     }
 
     nlwSetup.addDay("01/01")
+    nlwSetup.addDay("02/01")
+    nlwSetup.addDay("03/01")
+    nlwSetup.addDay("04/01")
+    nlwSetup.addDay("05/01")
+    nlwSetup.addDay("06/01")
+    nlwSetup.addDay("07/01")
+    nlwSetup.addDay("08/01")
+    nlwSetup.addDay("09/01")
+    nlwSetup.addDay("10/01")
+    nlwSetup.addDay("11/01")
+    nlwSetup.addDay("12/01")
+    nlwSetup.addDay("13/01")
+    nlwSetup.addDay("14/01")
+    nlwSetup.addDay("15/01")
+    nlwSetup.addDay("16/01")
+    nlwSetup.addDay("17/01")
+    nlwSetup.addDay("18/01")
+    nlwSetup.addDay("19/01")
+    nlwSetup.addDay("20/01")
+    nlwSetup.addDay("21/01")
+    nlwSetup.addDay("22/01")
+    nlwSetup.addDay("23/01")
+    nlwSetup.addDay("24/01")
+    nlwSetup.addDay("25/01")
+    nlwSetup.addDay("26/01")
+    nlwSetup.addDay("27/01")
+    nlwSetup.addDay("28/01")
+    nlwSetup.addDay("29/01")
+    nlwSetup.addDay("30/01")
+    nlwSetup.addDay("31/01")
+
 }
 
 function save() {
